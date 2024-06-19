0422ab802912c6129abf55f69a678df914449616
Mohammed Faizan Ghani <mohammedfaizanghani78@gmail.com>
2024-06-20 01:43:57 +0530

    json data added

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3001 
 
+const data = {
+    "employees":[
+        {"firstName":"John", "lastName":"Doe"},
+        {"firstName":"Anna", "lastName":"Smith"},
+        {"firstName":"Peter", "lastName":"Jones"}
+    ]
+    }
+
 app.get('/', (req, res) => {
   res.send('Mohammed Faizan Ghani')
 })
@@ -12,6 +20,10 @@ app.get('/Login', (req, res) => {
     res.send('User Login')
   })
 
+  
+app.get('/data', (req, res) => {
+    res.json(data)
+  })
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
